a929f82127d7052dc20e585464f7d9e7d94ecc3c
Adrian Antanon <adrian.antanon@festo.com>
2023-01-19 11:57:47 +0100

    TEST: add new tests on output file

--- basic-testing-frontend/app.js
+++ basic-testing-frontend/app.js
@@ -1,44 +1,19 @@
-import { extractNumbers } from './src/parser.js';
-import {
-  validateStringNotEmpty,
-  validateNumber,
-} from './src/util/validation.js';
-import { add } from './src/math.js';
-import { transformToNumber } from './src/util/numbers.js';
-
+import { calculateResult } from './src/math';
+import { validateResult } from './src/util/validation';
+import { outputResult } from './src/output.js';
+import { extractEnteredNumberValues } from './src/parser';
 const form = document.querySelector('form');
-const output = document.getElementById('result');
 
 function formSubmitHandler(event) {
   event.preventDefault();
-  const formData = new FormData(form);
-  const numberInputs = extractNumbers(formData);
 
-  let result = '';
-  const test = '';
+  const numberValues = extractEnteredNumberValues(form);
 
-  try {
-    const numbers = [];
-    for (const numberInput of numberInputs) {
-      validateStringNotEmpty(numberInput);
-      const number = transformToNumber(numberInput);
-      validateNumber(number);
-      numbers.push(number);
-    }
-    result = add(numbers).toString();
-  } catch (error) {
-    result = error.message;
-  }
+  const result = calculateResult(numberValues);
 
-  let resultText = '';
+  const resultText = validateResult(result);
 
-  if (result === 'invalid') {
-    resultText = 'Invalid input. You must enter valid numbers.';
-  } else if (result !== 'no-calc') {
-    resultText = 'Result: ' + result;
-  }
-
-  output.textContent = resultText;
+  outputResult(resultText);
 }
 
 form.addEventListener('submit', formSubmitHandler);
